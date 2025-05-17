6cd500e3d30727d85989529d914f991770cfbe32
unknown <celajtea@gmail.com>
2025-05-17 12:10:43 +0200

    Spring starter setup

---
+++ src/main/java/com/e_commerce/Inventory/Managment/system/InventoryManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.e_commerce.Inventory.Managment.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryManagmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryManagmentSystemApplication.class, args);
+	}
+
+}
